e9743808301cf0bfba746b1f25a8f8aa3628e362
Ivan Antoschuk <antoschukivan2010@gmail.com>
2026-08-22 14:06:26 +0300

    fix eerro

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/e-commerce-react',
+  base: '/e-commerce-react/',
   plugins: [react(), tailwindcss()],
 })
